ad8cd37d4a3c46105611dd11447b9d2d15d6be40
EduardoSantanaSeverino <eduardoamparos@gmail.com>
2022-07-21 16:14:33 -0400

    Adding Swagger

--- TodoAPI/Startup.cs
+++ TodoAPI/Startup.cs
@@ -39,12 +39,12 @@ namespace TodoAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoAPI v1"));
-            }
+            //}
 
             app.UseRouting();
 
